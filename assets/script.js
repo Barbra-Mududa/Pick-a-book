f9b0b1fbd76bf12aff19bfa87ddbe4a9b1f76261
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-09-01 22:18:30 +0300

    image placement

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,47 @@
 const base_url ="http://localhost:3000"
-const books_url = "http://localhost:3000/books"
+const books1 = "http://localhost:3000/book1"
+const books2 = "http://localhost:3000/book2"
 
 
-const popularBooks = document.getElementsByClassName("popular-books")
-const updateBooks = (books) => {
-  books.forEach
-}
-const imageContainer = document.querySelectorAll(".img")
-const booksData= async () => {
-    return fetch(books_url).then(res => res.json())
-}   
+// const popularBooks = document.getElementsByClassName("popular-books")
+// const updateBooks = (books) => {
+//   books.forEach
+// }
+// const imageContainer = document.querySelectorAll(".img")
+// const booksData= async () => {
+//     return fetch(books_url).then(res => res.json())
+// }   
 
-document.addEventListener("DOMContentLoaded", async () => {
-    booksData();
-    let books = [];
+// document.addEventListener("DOMContentLoaded", async () => {
+//     booksData();
+//     let books = [];
 
-    books = await booksData();
-    console.log(books)
-    const updateBooks = (books) =>{
-    const data = books.map((_element, _index) =>{
-        console.log(_element.image)
-        console.log(imageContainer)
-        imageContainer.src= `${_element.image}`
-    })
-}
-updateBooks(books)
-});
+//     books = await booksData();
+//     console.log(books)
+//     const updateBooks = (books) =>{
+//     const data = books.map((_element, _index) =>{
+//         console.log(_element.image)
+//         console.log(imageContainer)
+//         imageContainer.src= `${_element.image}`
+//     })
+// }
+// updateBooks(books)
+// });
+
+const book1 = fetch(books1)
+.then(res => res.json())
+.then((data) => {
+  const image1 = document.querySelector('#card-image').src = data[0].image
+  const text1 = document.querySelector('#title').innerHTML = data[0].title
+})
+const book2 = fetch(books2)
+.then(res => res.json())
+.then((data1) => {
+  const image1 = document.querySelector('#card-image1').src = data1[0].image
+  const text1 = document.querySelector('#title1').innerHTML = data1[0].title
+})
+
+
 
 
 // const addLikes = document.getElementById("like-count")
@@ -38,6 +54,7 @@ updateBooks(books)
 //   document.getElementById("like-count").textContent = `${likes} likes`;
 // }
 // })
+// let like1=0;
 // document.getElementById("like-button1").addEventListener("click",() =>{
 //   likes += 1;
 //   updateLikes1();
