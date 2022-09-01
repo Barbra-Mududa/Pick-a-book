d675c55369cd008beb6281a34e1bed96b3eae801
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-09-01 20:49:13 +0300

    like-button functionality

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,56 @@
 const base_url ="http://localhost:3000"
-const image0 ="http://localhost:3000/image0"
-const image1 ="http://localhost:3000/image2"
-const image2 ="http://localhost:3000/image3"
-const reviews ="http://localhost:3000/reviews"
+const books_url = "http://localhost:3000/books"
 
-const imageHolder = document.getElementById("card-image");
-const reviewlist = document.getElementById("review-list");
-const likeButton = document.getElementById("like-button");
-const reviewForm = document.getElementById("review-form");
-const inputField = document.getElementById("review-input");
 
-let books=[];
-const booksData1 = async () => {
-    const response = await fetch(image0)
-    const data = await response.json()
-    document.querySelector(".image0").src = data[0].image1
+const popularBooks = document.getElementsByClassName("popular-books")
+const updateBooks = (books) => {
+  books.forEach
 }
+const imageContainer = document.querySelectorAll(".img")
+const booksData= async () => {
+    return fetch(books_url).then(res => res.json())
+}   
+
 document.addEventListener("DOMContentLoaded", async () => {
-    books = await booksData1();
-    console.log("image1:",booksData1);
-})
+    booksData();
+    let books = [];
 
-const addLikes = document.getElementById("like-count");
-let likes = 0;
-document.querySelector(".image0").addEventListener("click", () =>{
-    likes += 1;
-    renderLikes();
-})
-function renderLikes() {
-    document.getElementById("like-count").textContent = `${likes} likes`;
+    books = await booksData();
+    console.log(books)
+    const updateBooks = (books) =>{
+    const data = books.map((_element, _index) =>{
+        console.log(_element.image)
+        console.log(imageContainer)
+        imageContainer.src= `${_element.image}`
+    })
 }
-document.addEventListener("DOMContentLoaded", ()=>{
-    booksData1();
-})  
+updateBooks(books)
+});
+
+
+// const addLikes = document.getElementById("like-count")
+// let likes = 0;
+// document.getElementById("like-button").addEventListener("click",() =>{
+//   likes += 1;
+//   updateLikes();
+
+// function updateLikes(){
+//   document.getElementById("like-count").textContent = `${likes} likes`;
+// }
+// })
+// document.getElementById("like-button1").addEventListener("click",() =>{
+//   likes += 1;
+//   updateLikes1();
+
+// function updateLikes1(){
+//   document.getElementById("like-count1").textContent = `${likes} likes`;
+// }
+// })
+// document.getElementById("like-button2").addEventListener("click",() =>{
+//   likes += 1;
+//   updateLikes1();
+
+// function updateLikes1(){
+//   document.getElementById("like-count2").textContent = `${likes} likes`;
+// }
+// })
